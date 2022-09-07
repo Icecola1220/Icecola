6fa4d9cb4a3c7595102209c00f8798b05cef6733
Icecola1220 <38715011+Icecola1220@users.noreply.github.com>
2022-09-08 00:31:55 +0800

    Update binance_pool.py
    丧心病狂的，找到了真正的bug，这个版本算是最稳定完善的版本了。第一期这个版本算是搭建完成。后续也都是基于这个版本来开发，后续优化的工作，后面在说了。

--- binance_pool.py
+++ binance_pool.py
@@ -1,18 +1,17 @@
 import requests
 import logging
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service  # 导入service服务进程
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 # import time  #用于对请求加延时，爬取速度太快容易被反爬
 from time import sleep  # 同上
 import time  # 用与获取实时时间
 
-start_time = time.time()  # 程序开始时间
 # 创建错误日志记录
 logging.basicConfig(filename="Error.log", filemode="w", format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
                     datefmt="%d-%M-%Y %H:%M:%S", level=logging.ERROR)
-
-crawler_url = 'https://www.binance.com/zh-CN/swap/pool'  # 设置需要爬的链接
 # 基本浏览器设置
 chrome_options = Options()
 chrome_options.add_argument('--no-sandbox')  # 解决DevToolsActivePort文件不存在的报错
@@ -21,23 +20,17 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('blink-settings=imagesEnabled=false')  # 不加载图片, 提升速度
 chrome_options.add_argument('--hide-scrollbars')  # 隐藏滚动条, 应对一些特殊页面
 
-
-# 设置实例化浏览器，及设置
-
-# txxt = ''  # 初始化，第一条对比文本
-#txt = [1, 2, 3]
-#txt_emoji = [1, 2, 3]
-txt = ['','','','','','','','','','']
-txt_emoji = ['','','','','','','','','','']
-txt_def = []
+# 初始化crawler变量
+txt = ['', '', '', '', '', '', '', '', '', '']
+txt_emoji = ['', '', '', '', '', '', '', '', '', '']
 
 
 def telegram_send(message):
     # Telegram_API链接信息配置
     bot_token = '5695810571:AAHUkIGjCwDMFQWLBzADRLN54IWqvtd2Kwg'  # Telegram_bot私钥
     bot_chatID = 'icecola_news'  # 频道名称
-    #bot_token = '5321232286:AAGIDNJJZsSJrOFvuBR6tk3zMqn30_qagn0'  # Telegram_bot私钥
-    #bot_chatID = 'icecola_new1'  # 频道名称
+    # bot_token = '5321232286:AAGIDNJJZsSJrOFvuBR6tk3zMqn30_qagn0'  # Telegram_bot私钥
+    # bot_chatID = 'icecola_new1'  # 频道名称
     # bot_message ="Testing"
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=@' + bot_chatID + '&text=' + message
     # print(send_text)
@@ -45,15 +38,39 @@ def telegram_send(message):
     # print(response)
 
 
+# def token_price(token_name):
+#     #browser = webdriver.Chrome("/Users/icecola/Documents/我的知识体系/Python改变世界/chromedriver",options=chrome_options)
+#     browser = webdriver.Chrome('/bin/chromedriver', options=chrome_options)  # linux运行地址
+#     browser.set_window_size(1440, 900)  # 设置屏幕大小，不同大小，展示样式都不同，需要注意
+#     browser.get('https://www.coingecko.com/en/searchbox')  # 打开要爬取的网页
+#     sleep(1)
+#     # 输入要查询的token
+#     search = browser.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div/input")  # 获取收益按钮")  # 获取收益按钮
+#     sleep(1)
+#     search.send_keys(token_name)
+#     sleep(1)
+#     search.send_keys(Keys.ENTER)
+#
+#     # 获取价格
+#     onehour = browser.find_element(By.XPATH, '//*[@id="general"]/div[1]/div[1]/div[2]/div[2]/div[1]/span').text
+#     oneday = browser.find_element(By.XPATH, '//*[@id="general"]/div[1]/div[1]/div[2]/div[2]/div[2]/span').text
+#     oneweek = browser.find_element(By.XPATH, '//*[@id="general"]/div[1]/div[1]/div[2]/div[2]/div[3]/span').text
+#
+#     browser.close()
+#     return onehour, oneday, oneweek
+
+
 # 爬取币安poolTop10数据
 def crawler():
-    text = ""
-    #browser = webdriver.Chrome("/Users/icecola/Documents/我的知识体系/Python改变世界/chromedriver", options=chrome_options)
+    On_Off = False  #判定是否发送消息
+    c_service = Service('/bin/chromedriver')  # 引入chromedriver服务进程
+    c_service.command_line_args()
+    c_service.start()
+    # browser = webdriver.Chrome("/Users/icecola/Documents/我的知识体系/Python改变世界/chromedriver",options=chrome_options)
     browser = webdriver.Chrome('/bin/chromedriver', options=chrome_options)  # linux运行地址
     browser.set_window_size(1440, 900)  # 设置屏幕大小，不同大小，展示样式都不同，需要注意
     # browser.maximize_window  #设置最大化浏览器
-
-    browser.get(crawler_url)  # 打开要爬取的网页
+    browser.get('https://www.binance.com/zh-CN/swap/pool')  # 打开要爬取的网页
     sleep(1)
 
     name = browser.find_element(By.XPATH,
@@ -85,52 +102,57 @@ def crawler():
 
         if txt[page - 1] == text:
             txt_emoji[page - 1] = text
+
         else:
             txt[page - 1] = text
             txt_emoji[page - 1] = '✅' + text
-        # print(text)
+            On_Off = True
+
+        # # 添加token_price波动值
+        # onehour, oneday, oneweek = token_price(value_name.split("/")[0])
+        # txt[page - 1] += f'  {onehour}, {oneday}, {oneweek}'+'\n'
+        # txt_emoji[page - 1] += f'  {onehour}, {oneday}, {oneweek}'+'\n'
+
         # point = browser.find_element(By.XPATH, page_mint_value) #流动性挖矿量
         # print(point.text)
         # point = browser.find_element(By.XPATH, page_shouyi)  #收益率
         # print(point.text)
         # point = browser.find_element(By.XPATH, jiaoyiliang)    #交易量
         # print(point.text)
-    loct = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    end_time = time.time()
-    end_time = (end_time - start_time) / 60
-    print("耗时: {:.2f}分钟".format(end_time))
-    #print(text)
-    browser.quit()
+
+    # print(text)
     # return '第' + str(i) + '次' + ':' + loct + '\n' + text
-    return txt, txt_emoji
+    tt = ""
+    if On_Off:
+        for t in txt_emoji:
+            tt += t
+        telegram_send(tt)
+    else:
+        pass
+    print(txt_emoji)
+    browser.quit()
+    c_service.stop()
 
 
 if __name__ == "__main__":
     i = 0  # 循环数值
     while True:
+        # start_time = time.time()  # 程序开始时间
+        loct = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())  # 打印即时时间
+        print(loct)
         i += 1
-        print('第', i, '次开始运行：')
+        # print('第', i, '次开始运行：')
         try:
-            txt, txt_emoji = crawler()
-
-            tt = "" #tt为循环，从列表中拉出文本
-            if txt_def != txt:
-                txt_def = txt
-                for t in txt_emoji:
-                    tt += t
-                telegram_send(tt)
-                #print("发送成功")
-                #print(txt_emoji)
-            else:
-                #print("没有变化")
-                pass
-
+            crawler()
         except Exception as e:
             print('第', i, '次运行的错误')
             logging.exception(e)  # 捕获一个错误,并打印        continue
-        # crawler()
-        # telegram_send(str(i))
-        print('第', i, '次结束')
+
+        # end_time = time.time()
+        # end_time = end_time - start_time
+        # print("耗时: {:.2f}秒".format(end_time))
+
+        # print('第', i, '次结束')
         # sleep(10)
 
 # 抓去一次binance数据，存入x；
