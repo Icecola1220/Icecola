52444767671182b9e395106131d7bf2cff6b2e6c
Icecola1220 <38715011+Icecola1220@users.noreply.github.com>
2022-09-18 22:51:06 +0800

    Update db_twitter.py
    1.分别存入不同语言的数据库

--- Twitter_King/db_twitter.py
+++ Twitter_King/db_twitter.py
@@ -7,9 +7,10 @@ db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='icecola@1
 
 # linux数据库地址
 # db = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='icecola', db='kol', charset='utf8mb4')
+# 这个是查询函数，返回数据库里的uid
 def uidlistDB():
     cs1 = db.cursor()
-    sql1 = "SELECT uid FROM  twitter"
+    sql1 = "SELECT uid FROM  twitter_cn"
     cs1.execute(sql1)
     datalist = []
     alldata = cs1.fetchall()
@@ -23,15 +24,31 @@ def insertDB(Table_0, Table_1, Table_2, Table_3, Table_4, Table_5):
     try:
         Table_1 = Table_1.replace('"', "'")  # 对单双引号进行转换
         Table_5 = Table_5.replace('"', "'")
-        sql = 'insert into twitter values ("%s", "%s", "%s", "%s", "%s", "%s")' % (
-            Table_0, Table_1, Table_2, Table_3, Table_4, Table_5)
+
+        if Table_4 == 'CN':
+            sql = 'insert into twitter_cn values ("%s", "%s", "%s", "%s", "%s", "%s")' % (
+                Table_0, Table_1, Table_2, Table_3, Table_4, Table_5)
+        elif Table_4 == 'EN':
+            sql = 'insert into twitter_en values ("%s", "%s", "%s", "%s", "%s", "%s")' % (
+                Table_0, Table_1, Table_2, Table_3, Table_4, Table_5)
+        elif Table_4 == 'JP':
+            sql = 'insert into twitter_jp values ("%s", "%s", "%s", "%s", "%s", "%s")' % (
+                Table_0, Table_1, Table_2, Table_3, Table_4, Table_5)
+        elif Table_4 == 'KO':
+            sql = 'insert into twitter_ko values ("%s", "%s", "%s", "%s", "%s", "%s")' % (
+                Table_0, Table_1, Table_2, Table_3, Table_4, Table_5)
+        elif Table_4 == 'Unknown':
+            sql = 'insert into twitter_other values ("%s", "%s", "%s", "%s", "%s", "%s")' % (
+                Table_0, Table_1, Table_2, Table_3, Table_4, Table_5)
         db.cursor().execute(sql)
         db.commit()
 
     except Exception as e:
         db.rollback()
         print(str(e))
-#("uid","推特名称","主动关注数","粉丝数","是否中文","个人简介")
+
+
+# ("uid","推特名称","主动关注数","粉丝数","是否中文","个人简介")
 # a = 111
 # b = ' 陳威廉 '
 # c = 123
@@ -39,6 +56,6 @@ def insertDB(Table_0, Table_1, Table_2, Table_3, Table_4, Table_5):
 # f = 'FALSE'
 # e = 'The leading DEX Aggregator, offering the best swap returns across 17 chains. Join us: https://t.co/SK3ljHPZkk'
 #
-# aa = 123
-# #sql = "insert into color(url, time) values('%s','%s')" % (Url，Time)
-# insertDB(a,b,c,f,d,e)
+# # sql = "insert into color(url, time) values('%s','%s')" % (Url，Time)
+# aa = [a, b, c, f, d, e]
+# insertDB(aa)
